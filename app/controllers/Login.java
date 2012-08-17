3a4c48829dd9f7da81b5a2781734d2c56e543753
tomnatt <github@tomnatt.com>
2012-08-17 17:10:20 +0100

    refactor

--- app/controllers/Login.java
+++ app/controllers/Login.java
@@ -9,13 +9,44 @@ import java.util.*;
 
 import models.*;
 
-public class Security extends Controller {
+public class Login extends Controller {
 
     public static void login() {
         render();
     }
 
     public static void authenticate(String user) {
+
+        if (OpenID.isAuthenticationResponse()) {
+
+            UserInfo verifiedUser = OpenID.getVerifiedID();
+
+            if (User.findByUserID(verifiedUser.id) == null) {
+                // user doesn't exist, fail auth
+                verifiedUser = null;
+            }
+
+            if (verifiedUser == null) {
+                flash.put("error", "failed");
+                login();
+            }
+            session.put("user", verifiedUser.id);
+            Application.index(null);
+
+        } else {
+            OpenID.id(user).verify(); // will redirect the user
+        }
+    }
+
+    // new user
+
+    public static void newUser() {
+        render();
+    }
+
+    public static void newUserAuthenticate(String user) {
+
+        // TODO something different to this
 
         if (OpenID.isAuthenticationResponse()) {
 
